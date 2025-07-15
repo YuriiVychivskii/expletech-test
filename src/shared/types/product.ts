f8123c772dd8b80f6d804c6e4e114d880382bc7e
Yura Vyvhivskii <yuravychivskii@gmail.com>
2025-07-15 17:15:31 +0300

    refactor: add null to catagory types, price string => number, add loading, error, filters to product state types

--- src/shared/types/product.ts
+++ src/shared/types/product.ts
@@ -1,14 +1,29 @@
-export type ProductCategory = 'Electronics' | 'Clothing' | 'Home' | 'Sports'
+export type ProductCategory =
+  | 'Electronics'
+  | 'Clothing'
+  | 'Home'
+  | 'Sports'
+  | null
 
 export type Product = {
   id: number
   name: string
   image: string
-  price: string
+  price: number
   category: ProductCategory
   description: string
 }
 
 export type ProductState = {
   products: Product[]
+  loading: boolean
+  error: string | null
+  filters: {
+    category: ProductCategory
+    searchQuery: string
+    priceFilter: {
+      min: number
+      max: number
+    }
+  }
 }
