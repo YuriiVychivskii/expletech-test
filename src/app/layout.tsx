4092a49a243f48bc6b2ff56a5b24c51e51f91369
Yura Vyvhivskii <yuravychivskii@gmail.com>
2025-07-16 12:24:13 +0300

    feat: add redux provider and change metadata, font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,29 @@
-import type { Metadata } from 'next';
-import { Poppins } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import './globals.css'
+import Providers from './providers'
 
 const poppins = Poppins({
-	variable: '--font-poppins',
-	subsets: ['latin'],
-	weight: ['300', '500', '700', '900'],
-});
+  variable: '--font-poppins',
+  subsets: ['latin'],
+  weight: ['300', '500', '700', '900'],
+})
 
 export const metadata: Metadata = {
-	title: 'Expletech',
-	description: 'Testing project',
-};
+  title: 'Expletech',
+  description: 'Testing project',
+}
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode
 }>) {
-	return (
-		<html lang='en'>
-			<body className={`${poppins.variable} antialiased`}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} antialiased`}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
 }
