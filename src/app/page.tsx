c1ec62de638bbe6a6914a0e8798df860680874a4
Yura Vyvhivskii <yuravychivskii@gmail.com>
2025-07-16 16:41:50 +0300

    feat: app assembled with header, sidebar and product list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import ProductList from '@/entities/products/ui/product-list'
+import Header from '@/widgets/header'
+import Sidebar from '@/widgets/sidebar'
+
 export default function Home() {
-	return <div></div>;
+  return (
+    <div className="flex flex-col">
+      <Header />
+      <div className="container flex flex-col items-center gap-5 pt-24 sm:items-start md:flex-row">
+        <Sidebar />
+        <ProductList />
+      </div>
+    </div>
+  )
 }
